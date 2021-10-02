24b3037299a45a32ad1a0406129a762e4019fe8f
IiIymik <sambotemp@gmail.com>
2021-10-02 22:38:13 +0300

    add file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.css';
+
+
+ReactDOM.render(
+  <h1>Hello from js</h1>,
+  document.querySelector(
+    '#root'
+  )
+);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PORT_DEV_SERVE = 2828;
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        port: PORT_DEV_SERVE,
+        hot: true,
+        liveReload: false,
+        open: true,
+    },
+    module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+            },
+            {
+                test: /\.css$/,
+                use: [
+        'style-loader',
+        'css-loader'
+      ]
+        }
+    ],
+  },
+    plugins: [
+        
+            new HtmlWebpackPlugin({
+                template:'./public/index.html',
+            })
+        
+    ]
+};
